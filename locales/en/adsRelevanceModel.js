a0e9848d53f9c96661bf1208ec68f8ed21a63186
Panos <ptigas@brave.com>
2018-07-27 13:13:23 +0100

    added again logistic regression models

---
+++ locales/en/adsRelevanceModel.js
@@ -0,0 +1,5 @@
+module.exports={
+    names:["short_term_category_match", "intent_category_match", "long_term_category_match", "winning_over_time_match"], // features
+    weights:[0.5, 0.5, 0.0, 1.0],
+    intercept: 0.0
+}
